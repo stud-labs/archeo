6628d695033536e0f55ee3bb7015fb7ba892ff85
Evgeny Cherkashin <eugeenai@irnok.net>
2023-04-28 17:12:22 +0000

    Initial import

---
+++ src/test/java/edu/isu/archeo/AppTest.java
@@ -0,0 +1,20 @@
+package edu.isu.archeo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
